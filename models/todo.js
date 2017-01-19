3d8b1eb839d95a7aee8d70cf54418cbfe9490a37
Samue PH Sinaga <samuelphsinaga@gmail.com>
2017-01-19 17:54:04 +0700

    done

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var todo = sequelize.define('todo', {
+    task: DataTypes.STRING,
+    status: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        
+      }
+    }
+  });
+  return todo;
+};
