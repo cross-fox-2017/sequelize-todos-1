3e635b4e25babfbbff954e25d1dd0e551401a2dc
timocodex <timothy.og.siahaan@gmail.com>
2017-01-19 14:58:05 +0700

    selesai setelah dikerjain

---
+++ seeders/20170119042733-entry-todo.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+    Add altering commands here.
+    Return a promise to correctly handle asynchronicity.
+
+    Example:
+    return queryInterface.bulkInsert('Person', [{
+    name: 'John Doe',
+    isBetaMember: false
+  }], {});
+  */
+
+  return queryInterface.bulkInsert("Todos",[
+    {
+      task:'bangun',
+      completed:0,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    },
+    {
+      task:'makan',
+      completed:0,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    },
+    {
+      task:'coding',
+      completed:0,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    }
+  ],{});
+},
+
+down: function (queryInterface, Sequelize) {
+  /*
+  Add reverting commands here.
+  Return a promise to correctly handle asynchronicity.
+
+  Example:
+  return queryInterface.bulkDelete('Person', null, {});
+  */
+}
+};
